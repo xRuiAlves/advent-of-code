0086557b98f794ffd243b22fe761cdecfed64829
xRuiAlves <ruialves.esrt.98@gmail.com>
2024-01-09 21:23:45 +0000

    Refactor 2018 day 22

--- 2018-scala/Day22.scala
+++ 2018-scala/Day22.scala
@@ -14,9 +14,15 @@ object Day22 {
   private[this] final val ErosionLevelModulo = 20183
   private[this] final val TypeModulo = 3
 
+  enum Type(val erosion: Int):
+    case Rocky extends Type(0)
+    case Wet extends Type(1)
+    case Narrow extends Type(2)
+
+  type Map = Array[Array[Int]]
+
   def main(args: Array[String]): Unit = {
     val input = Input.fromArray(readResourceLines("day22.txt"))
-    println(input)
     val geologicIndexes = mutable.Map[Coord, Int]()
     val start = Coord(0, 0)
 
@@ -34,14 +40,14 @@ object Day22 {
 
     def getErosionLevel(coord: Coord): Int = (getGeologicIndex(coord) + input.depth) % ErosionLevelModulo
 
-    def getRegionType(coord: Coord): Int = getErosionLevel(coord) % TypeModulo
+    def getRegionType(coord: Coord): Type = Type.fromOrdinal(getErosionLevel(coord) % TypeModulo)
 
-    val map = Array.ofDim[Int](input.target.y + 1, input.target.x + 1)
+    val map = Array.ofDim[Type](input.target.y + 1, input.target.x + 1)
     for (y <- map.indices; x <- map(y).indices) {
       map(y)(x) = getRegionType(Coord(x, y))
     }
 
-    val part1 = map.map(_.sum).sum
+    val part1 = map.map(_.map(_.erosion).sum).sum
     val part2 = 0
 
     println(s"Part 1: $part1")
@@ -60,4 +66,5 @@ object Day22 {
       )
     }
   }
+
 }
